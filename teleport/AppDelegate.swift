6394812d9cba4fef7a58571b11924f2d6db599c3
barsbold-b <barsbold.coding@gmail.com>
2023-08-19 23:13:01 +0800

    Initial Commit

---
+++ teleport/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  teleport
+//
+//  Created by Barsbold Bayarerdene on 8/19/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
